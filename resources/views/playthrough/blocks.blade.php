889c1c4fe6f0955996977c121cbfcb60f9cd03a2
Jesse Rice <hobstweedl@gmail.com>
2015-07-17 15:46:35 -0400

    Playthrough card updates and fixes
    Still a bug with two players not working correctly, maybe swap views
    based on total. IDK

--- resources/views/playthrough/blocks.blade.php
+++ resources/views/playthrough/blocks.blade.php
@@ -1,27 +1,58 @@
 <div class="row">
     @foreach($plays as $p)    
         <div class="col-md-3 col-sm-12 col-xs-12">
+            <div class="card">
+                <div class="banner text-center">
+                    {{ Helper::prettyDate( $p->date_played ) }}
+                </div>
 
 
-            <div class="flip-cardd">
-                <div class="front">
-                <img class="img-responsive opacity" src="{{ asset('games/'.camel_case($p->game->name).'.jpg') }}">
+                <div class="game-tile" style="height: 360px; width: 100%; background-image: url('{{ asset('games/'.camel_case($p->game->name).'.jpg') }}') ">
 
-                    <div class="abs-pos">
-                        <div class="row text-center">
-                            <div class="col-md-12">
-                                <h3>{{ Helper::prettyDate( $p->date_played ) }}</h3>
-                                <div class="player-listing">
-                                @foreach($p->players as $i)
-                                    <img src="{{asset('people/'.$i->player_id.'-thumbnail.jpg') }}" class="img-circle rankings">
-                                    {{ $i->score}}
-                                @endforeach
+                <?php $playercount = count($p->players); ?>
+
+                    <div class="banner banner-bottom">
+                        @foreach($p->players as $k => $i)
+
+                            @if( $k == 0 )
+                                <div class="row">
+                                    <div class="col-md-12 col-sm-12 col-xs-12 text-center">
+                                        <img src="{{asset('people/'.$i->player_id.'.jpg') }}" class="img-circle rankings rank-1">
+                                    </div>
                                 </div>
-                                <h3 class="text-center">{{ $p->notes }} </h3>
-                            </div>
-                        </div>
+                                
+                            @elseif( $k == 1 )
+                                    @if($playercount >= 3)
+                                    <div class="row">
+                                    @endif
+                                    <div class="col-md-6 text-center">
+                                        <img src="{{asset('people/'.$i->player_id.'.jpg') }}" class="img-circle rankings rank-2">
+                                    </div>
+                                
+                                
+                            @elseif( $k == 2 )
+                                        <div class="col-md-6 text-center">
+                                            <img src="{{asset('people/'.$i->player_id.'.jpg') }}" class="img-circle rankings rank-3">
+                                        </div>
+                                            @if($playercount >= 3)
+                                                </div>
+                                            @endif
+
+                                
+                            
+                            @else
+                                <div class="row">
+                                    <div class="col-md-12 text-center">
+                                        <img src="{{asset('people/'.$i->player_id.'.jpg') }}" class="img-circle rankings">
+                                    </div>
+                                </div>
+                                
+                            @endif
+                            
+                        @endforeach
                     </div>
                 </div>
+
             </div>
         </div>
 
@@ -30,14 +61,16 @@
 
 <script>
     $( document ).ready(function() {
+            /*
             var cards = $(".flip-cardd").find('.player-listing');
 
             $.each(cards, function(i, v){
-                var images = $(v).find('img').length;
+                
+                var images = $(v).find('img');
                 console.log(images);
                 console.log('end');
             })
-               
-
+            */
+            
     });
 </script>
